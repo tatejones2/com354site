a19b68a387856c635a7439485e54c83e13be3743
Tate Jones <tnj@njit.edu>
2026-02-04 10:43:14 -0500

    Remove LinkedIn and Twitter social links from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,6 @@ export default function Footer() {
         <p>&copy; {currentYear} Tate Jones. All rights reserved.</p>
         <div className="social-links">
           <a href="#" className="social-link">GitHub</a>
-          <a href="#" className="social-link">LinkedIn</a>
-          <a href="#" className="social-link">Twitter</a>
         </div>
       </div>
     </footer>
